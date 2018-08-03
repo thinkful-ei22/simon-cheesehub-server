5e41fa0657007f88a230f515bc59da6d278b4731
Simon Chae <s13chae@gmail.com>
2018-08-03 08:32:44 -0700

    get endpoint

--- index.js
+++ index.js
@@ -33,6 +33,30 @@ function runServer(port = PORT) {
     });
 }
 
+app.get('/api/cheeses', (req, res) => {
+  res.json(
+    [
+      'Bath Blue',
+      'Barkham Blue',
+      'Buxton Blue',
+      'Cheshire Blue',
+      'Devon Blue',
+      'Dorset Blue Vinney',
+      'Dovedale',
+      'Exmoor Blue',
+      'Harbourne Blue',
+      'Lanark Blue',
+      'Lymeswold',
+      'Oxford Blue',
+      'Shropshire Blue',
+      'Stichelton',
+      'Stilton',
+      'Blue Wensleydale',
+      'Yorkshire Blue'
+    ]
+  );
+});
+
 if (require.main === module) {
   dbConnect();
   runServer();
